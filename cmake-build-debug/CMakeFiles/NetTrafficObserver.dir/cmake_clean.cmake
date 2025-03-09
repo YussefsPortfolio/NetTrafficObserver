ceed58c62113754b0298246d459939bc2456b445
Yussef888 <115888298+iLoveMyPortfolio@users.noreply.github.com>
2025-03-09 06:39:32 +0100

    New essential Features and easier terminal

---
+++ cmake-build-debug/CMakeFiles/NetTrafficObserver.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NetTrafficObserver.dir/PacketSniffer.cpp.o"
+  "CMakeFiles/NetTrafficObserver.dir/PacketSniffer.cpp.o.d"
+  "CMakeFiles/NetTrafficObserver.dir/main.cpp.o"
+  "CMakeFiles/NetTrafficObserver.dir/main.cpp.o.d"
+  "NetTrafficObserver"
+  "NetTrafficObserver.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NetTrafficObserver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
